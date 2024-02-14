955d2f611462f246fbc08c4907dce54ef7c42631
somap98 <128257752+somap98@users.noreply.github.com>
2024-02-14 17:05:43 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -1,45 +1,63 @@
-#include "mymalloc.h"
 #include <stdio.h>
+#include "mymalloc.h"
+
 
 #define MEMLENGTH 512
 static double memory[MEMLENGTH];
 
-typedef struct Metadata {
+typedef struct Header {
     size_t size;
     size_t payload;
     int free;
-} Metadata;
+} Header;
+//since both variable types are integers, both are initialized to 0
 
 void *mymalloc(size_t size, char *file, int line) {
+
+    if(size == 0) {
+        return NULL;
+    }
     //somehow apparently rounds up to nearest 8 using bitwise
-    size_t allocated_space = ((size + sizeof(Metadata) +7) & ~7);
+    size_t allocated_space = ((size + sizeof(Header) +7) & ~7);
 
-    //pointer to beginning of memory
-    Metadata* current = (Metadata*)memory;
+    if(allocated_space >= MEMLENGTH*sizeof(memory)) {
+        return NULL;
+    }
 
+    //pointer to beginning of memory (all metadata is in linked list)
+    Header* current = (Header*)memory;
+
+    if(current->size == 0) {
+        current->size = MEMLENGTH * sizeof(memory);
+        current->payload = (MEMLENGTH * sizeof(memory)) - sizeof(Header);
+        current->free = 0;
+    }
     //while the address of current pointer is less then the bounds of memory (MEMLENGTH is how long memory array spans)
-    while(current < (Metadata*)(memory + MEMLENGTH)) {
-        if(current -> free == 0 && current -> size >= allocated_space) {
-            size_t extra_space = allocated_space - (current -> size);
+    // 0 + 8(512)
+    while(current < (Header*)(memory + MEMLENGTH)) {
+        if(current->free == 0 && current->payload >= allocated_space) {
+            size_t extra_space = current->payload - allocated_space;
 
-            if(extra_space >= sizeof(Metadata) + 1) {
-                Metadata* next_header = (Metadata*)((void*)current + allocated_space);
-                next_header -> size = extra_space;
-                next_header -> payload = extra_space - sizeof(Metadata);
-                next_header -> free = 0;
+            if(extra_space >= sizeof(Header) + 1) {
+                Header* next_header = (Header*)((char*)current + (sizeof(Header)+size));
+                next_header->size = extra_space;
+                next_header->payload = extra_space - sizeof(Header);
+                next_header->free = 0;
 
-                current -> size = allocated_space - extra_space;
-                current -> payload = (allocated_space - extra_space) - sizeof(Metadata);
+                current->size = allocated_space - extra_space;
+                current->payload = (allocated_space - extra_space) - sizeof(Header);
             } else {
-                current -> size = allocated_space;
-                current -> payload = allocated_space - sizeof(Metadata);
+                current->size = allocated_space;
+                current->payload = allocated_space - sizeof(Header);
             }
 
-            current -> free = 1;
-            return ((void*)current + sizeof(Metadata));
+            current->free = 1;
+            return ((char*)current + sizeof(Header));
         }
-
+        current = (Header*)((char*)current + current -> size);
     }
 
     return NULL;
 }
+
+
