4b85fb8d88472ad12c26fca081eef3adb4866721
somap98 <128257752+somap98@users.noreply.github.com>
2024-02-14 20:05:38 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -14,29 +14,39 @@ typedef struct Header {
 
 void *mymalloc(size_t size, char *file, int line) {
 
+    //if 0 bytes are requested
     if(size == 0) {
         return NULL;
     }
+
     //somehow apparently rounds up to nearest 8 using bitwise
     size_t allocated_space = ((size + sizeof(Header) +7) & ~7);
 
-    if(allocated_space >= MEMLENGTH*sizeof(memory)) {
+    //if number of bytes requested is greater the bytes available
+    if(allocated_space >= MEMLENGTH*sizeof(double)) {
         return NULL;
     }
 
-    //pointer to beginning of memory (all metadata is in linked list)
+    //pointer to beginning of memory 
     Header* current = (Header*)memory;
 
+
+    //initializing the first header
     if(current->size == 0) {
-        current->size = MEMLENGTH * sizeof(memory);
-        current->payload = (MEMLENGTH * sizeof(memory)) - sizeof(Header);
+        current->size = MEMLENGTH * sizeof(double);
+        current->payload = (MEMLENGTH * sizeof(double)) - sizeof(Header);
         current->free = 0;
     }
+
+
     //while the address of current pointer is less then the bounds of memory (MEMLENGTH is how long memory array spans)
     // 0 + 8(512)
     while(current < (Header*)(memory + MEMLENGTH)) {
+        //if header says space is free and the amount of bytes-header space is enough to allocate
         if(current->free == 0 && current->payload >= allocated_space) {
+            //extra space from, payload will always be >= space needed
             size_t extra_space = current->payload - allocated_space;
+
 
             if(extra_space >= sizeof(Header) + 1) {
                 Header* next_header = (Header*)((char*)current + (sizeof(Header)+size));
@@ -44,8 +54,8 @@ void *mymalloc(size_t size, char *file, int line) {
                 next_header->payload = extra_space - sizeof(Header);
                 next_header->free = 0;
 
-                current->size = allocated_space - extra_space;
-                current->payload = (allocated_space - extra_space) - sizeof(Header);
+                current->size = extra_space;
+                current->payload = extra_space - sizeof(Header);
             } else {
                 current->size = allocated_space;
                 current->payload = allocated_space - sizeof(Header);
@@ -54,7 +64,7 @@ void *mymalloc(size_t size, char *file, int line) {
             current->free = 1;
             return ((char*)current + sizeof(Header));
         }
-        current = (Header*)((char*)current + current -> size);
+        current = (Header*)((char*)current + current->size);
     }
 
     return NULL;
