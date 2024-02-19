a398dc3da5d1f533ece261046b2093ea16d53cf9
belwinjulian <82686592+belwinjulian@users.noreply.github.com>
2024-02-18 22:20:45 -0500

    Update mymalloc.c

--- mymalloc.c
+++ mymalloc.c
@@ -12,6 +12,19 @@ typedef struct Header {
 } Header;
 //since both variable types are integers, both are initialized to 0
 
+int isStartOfChunk(void* ptr) {
+    Header* current = (Header*)memory;
+    while(current < (Header*)(memory + MEMLENGTH)) {
+        if ((char*)current + sizeof(Header) == ptr) {
+            return 1;  // ptr is the start of a chunk
+        }
+        current = (Header*)((char*)current + current->size);
+        if (current->size == 0) break;
+    }
+    return 0;  // ptr is not the start of a chunk
+}
+
+
 void *mymalloc(size_t size, char *file, int line) {
 
     //if 0 bytes are requested
@@ -24,7 +37,7 @@ void *mymalloc(size_t size, char *file, int line) {
     size_t allocated_space = ((size + sizeof(Header) +7) & ~7);
 
     //if number of bytes requested is greater the bytes available
-    if(allocated_space > MEMLENGTH*sizeof(double)) {
+    if(allocated_space >= MEMLENGTH*sizeof(double)) {
          fprintf(stderr, "Memory Allocation Failed: Requested more than available space (%s:%d).\n", file, line);
         return NULL;
     }
@@ -39,17 +52,16 @@ void *mymalloc(size_t size, char *file, int line) {
         current->free = 0;
     }
 
-
-
+    
 
     //while the address of current pointer is less then the bounds of memory (MEMLENGTH is how long memory array spans)
     // 0 + 8(512)
     while(current < (Header*)(memory + MEMLENGTH)) {
         //if header says space is free and the amount of bytes-header space is enough to allocate
         
-        if(current->free == 0 && current->size >= allocated_space) {
+        if(current->free == 0 && current->payload >= allocated_space) {
             //extra space from, payload will always be >= space needed
-            size_t extra_space = current->size - allocated_space;
+            size_t extra_space = current->payload - allocated_space;
            
         //if there is enough extra space for header and atleast 1 byte, split the chunk (two headers)
        
@@ -69,7 +81,6 @@ void *mymalloc(size_t size, char *file, int line) {
             current->free = 1;
             //return address of payload (address of header + bytes header takes up)
             return ((char*)current + sizeof(Header));
-             
         }
         //traverse to next header
         current = (Header*)((char*)current + current->size);
@@ -100,25 +111,19 @@ void myfree(void *ptr, char *file, int line)
     // Get the header associated with this pointer
     Header* header = (Header*)((char*)ptr - sizeof(Header));
 
-
-// Error check: ensure ptr is at the start of a chunk
-    Header* temp = (Header*)memory;
-    int sum = 0;
-
-   /* while(temp < (Header*)(memory + MEMLENGTH)) {
-        temp = (Header*)((char*)temp + temp->size);
-        int sum = sum + temp->size;
-
-        if(ptr == (void*)((char*)temp+sizeof(Header))) {
-            break;
-        }
-        if(sum >= sizeof(double)*MEMLENGTH) {
+    // Error check: ensure ptr is at the start of a chunk
+    if(!(isStartOfChunk(ptr)))
+    {
         fprintf(stderr, "Error: Attempted to free an address not at the start of a chunk (%s:%d).\n", file, line);
         return;
-        }
-    } */
-
+    }
+// Error check: ensure ptr is at the start of a chunk
     
+    /*if (ptr != header->ptr) {
+        fprintf(stderr, "Error: Attempted to free an address not at the start of a chunk (%s:%d).\n", file, line);
+        return;
+    }
+    */
 
 
     if(header->free == 0)//make sure block is not already free
@@ -135,8 +140,8 @@ void myfree(void *ptr, char *file, int line)
     // Merge with next block if it is free
     Header* next = (Header*)((char*)header + header->size);
     if (next < (Header*)(memory + MEMLENGTH) && next->free == 0) {
-        header->size += next->size+6;
-        header->payload += next->size+6;
+        header->size += next->size+32;
+        header->payload += next->payload+64;
     }
 
     // Merge with previous block if it is free
@@ -149,7 +154,7 @@ void myfree(void *ptr, char *file, int line)
         current = (Header*)((char*)current + current->size);
     }
     if (prev && prev->free == 0) {
-        prev->size += header->size+6;
-        prev->payload += header->size+6;
-    } 
-} 
+        prev->size += header->size;
+        prev->payload += header->payload;
+    }
+}
